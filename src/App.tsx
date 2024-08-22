7f4c09fd887a5ce527b6d03d43a4e91cc04bf4da
Thalisson Castro <adaothalisson@hotmail.com>
2024-08-22 19:09:42 -0300

    REFACTOR: improve `App.tsx` to create a request for pokemon api

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
+import { useEffect, useState } from "react";
+
 import { Header } from "./components/Header";
-import { Card } from "./components/Card";
+import { Card, CardType } from "./components/Card";
+
+import { getPokemonList } from "./api/getPokemonList";
 
 import "./global.css";
 
+import styles from "./App.module.css";
+
 export function App() {
+  const [pokemonList, setPokemonList] = useState<CardType[]>([]);
+
+  useEffect(() => {
+    getPokemonList().then((data) => {
+      setPokemonList(
+        data.results.map((pokemon) => {
+          return {
+            pokemonName: pokemon.name,
+            url: pokemon.url,
+          };
+        })
+      );
+    });
+  }, []);
+
   return (
     <div>
       <Header />
-      <main>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <main className={styles.wrapper}>
+        {pokemonList.map((card: CardType) => {
+          return <Card card={card} />;
+        })}
       </main>
     </div>
   );
