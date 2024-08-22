f91fd4bfaaf06e2d5b6a4a2833907ecbce0208f6
Thalisson Castro <adaothalisson@hotmail.com>
2024-08-22 19:06:57 -0300

    FEAT: created the card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,25 @@
-export function Card() {
-  return <h1>Hello World</h1>;
+import styles from "./Card.module.css";
+
+export interface CardType {
+  // id: number;
+  pokemonName: string;
+  url: string;
+}
+
+interface CardProps {
+  card: CardType;
+}
+
+export function Card({ card }: CardProps) {
+  return (
+    <div className={styles.card}>
+      {/* <img src={card.url} /> */}
+      <div className={styles.description}>
+        <h3>
+          <b>{card.pokemonName}</b>
+        </h3>
+        <h4>{card.url}</h4>
+      </div>
+    </div>
+  );
 }
