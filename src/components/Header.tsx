7a39d949e09785998f74a79cd5a0422fa2ecb98b
Thalisson Castro <adaothalisson@hotmail.com>
2024-08-23 15:34:17 -0300

    FEAT: put a link in image header to send user to the home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 import logo from "../assets/pokebolalogo.png";
 
 import styles from "./Header.module.css";
@@ -5,7 +7,9 @@ import styles from "./Header.module.css";
 export function Header() {
   return (
     <header className={styles.header}>
-      <img src={logo} alt="logo do pokemon" />
+      <NavLink to={"/"} title="Página Inicial">
+        <img src={logo} alt="logo do pokemon" />
+      </NavLink>
     </header>
   );
 }
